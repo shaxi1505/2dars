5041e21b7417102d55dd451934ae400f13e6f059
shaxi1505 <86339091+shaxi1505@users.noreply.github.com>
2021-06-30 12:14:07 +0500

    Create 9-vazifa.py

---
+++ 9-vazifa.py
@@ -0,0 +1,7 @@
+
+9 . Istalgan sonni faktorialini topuvchi dastur Tuzing
+
+Шахзода Камолова, [29.06.21 12:08]
+# 4ni savoli : 4. City kalitini location ga almashtiring              # 4 ni javobi;  sampleDict = {"name": "Kelly", "age": 25, "salary": 8000, "city": "New york"} 
+sampleDict['location']=sampleDict.pop('city') 
+print(sampleDict)      Natijasi :  {'name': 'Kelly', 'age': 25, 'salary': 8000, 'location': 'New york'}
